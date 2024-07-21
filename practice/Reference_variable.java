6ece123ad0cdf56ec109641c6269592a3454dc4f
jisu217 <164330581+jisu217@users.noreply.github.com>
2024-07-21 15:09:52 +0900

    Update Reference_variable.java

--- practice/Reference_variable.java
+++ practice/Reference_variable.java
@@ -14,7 +14,6 @@ public class Reference_variable {
 
     Cake ca3 = new CheeseCake();
     CheeseCake ca4 = ca3;     // 불가능!
-
-// 이 시점에 컴파일러 및 가상머신은 ca3가 참조하는 대상을 Cake 인스턴스로 판단, ca3가 참조하는 인스턴스의 정확한 클래스 정보는 유지 X
+                              // 이 시점에 컴파일러 및 가상머신은 ca3가 참조하는 대상을 Cake 인스턴스로 판단, ca3가 참조하는 인스턴스의 정확한 클래스 정보는 유지 X
 
 }
