fbfae9a2f81e4a53c339388ef9502f3e983e81b6
jisu217 <164330581+jisu217@users.noreply.github.com>
2024-07-21 15:13:13 +0900

    Update inheritance.java

--- practice/inheritance.java
+++ practice/inheritance.java
@@ -17,9 +17,8 @@ public class inheritance {
                 System.out.printin(num); // 5와 7의 배수인 경우 실행
 
             }
+            
             System.out.println("count: " + count);
         }
-
     }
-
 }
