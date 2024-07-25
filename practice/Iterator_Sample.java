2b510c5d2eb7c6069792c4cc6c03df22ba8b9327
jisu217 <164330581+jisu217@users.noreply.github.com>
2024-07-25 15:33:11 +0900

    Iterator_Sample.java

--- practice/Iterator_Sample.java
+++ practice/Iterator_Sample.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class IteratorSample {
+public class Iterator_Sample {
     public static void main(String[] args) {
         ArrayList<String> myStringArrays = new ArrayList<String>();
 
